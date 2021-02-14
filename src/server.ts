4105c69fc490c2d294aee9336f0cc11bf069e334
Jeffrey Rose <jtr219@outlook.com>
2021-02-14 01:40:57 -0500

    add apollo-server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,32 @@
 import 'reflect-metadata'
 import dotenv from 'dotenv'
 import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { apolloLogger } from './utils/apolloLogger'
+
+/* ----------------------------- set up express ----------------------------- */
 
 dotenv.config()
 
 const main = async () => {
   const app = express()
+
+  /* -------------------------- set up apollo server -------------------------- */
+
+  /*
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [],
+      validate: false,
+    }),
+    plugins: [apolloLogger],
+    // context
+  })
+
+  apolloServer.applyMiddleware({ app })
+  */
+  /* ----------------------------- listen on port ----------------------------- */
 
   const port = process.env.LOCAL_PORT
 
