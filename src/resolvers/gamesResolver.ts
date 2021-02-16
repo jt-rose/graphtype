f3c4bb1f412909a20025edb74a3398beefa9f042
Jeffrey Rose <jtr219@outlook.com>
2021-02-16 00:41:13 -0500

    set up isAuth middleware

--- src/resolvers/gamesResolver.ts
+++ src/resolvers/gamesResolver.ts
@@ -1,9 +1,11 @@
 //import { GAMES } from './../entities/GAMES'
-import { Resolver, Query } from 'type-graphql'
+import { Resolver, Query, UseMiddleware } from 'type-graphql'
+import { isAuth } from './../middleware/isAuth'
 
 @Resolver()
 export class GamesResolver {
   @Query(() => String)
+  @UseMiddleware(isAuth)
   hello() {
     return 'apollo resolver online'
   }
